d68dc0243a8c2ec4447c30d06da5c03220e47464
rlauderdale <rjlauderdale26@gmail.com>
2018-04-30 21:36:58 -0500

    event handler
    added an event handler in StorePicker.js

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -6,11 +6,11 @@ import NotFound from './NotFound';
 
 const Router = () => (
     <BrowserRouter>
-    <Switch>
-        <Route exact path="/" component={StorePicker} />
-        <Route path="/store/:storeId" component={App} /> 
-        <Route component={NotFound} /> 
-    </Switch>
+        <Switch>
+            <Route exact path="/" component={StorePicker} />
+            <Route path="/store/:storeId" component={App} /> 
+            <Route component={NotFound} /> 
+        </Switch>
     </BrowserRouter>
 );
 
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { render} from 'react-dom';
+//import { render} from 'react-dom';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = event => {
+        //1. Stop the form from submitting
+        event.preventDefault();
+        //2. get the text from that input
+        const storeName = this.myInput.value.value;
+        //3. Change the page to /store/whatever-they-entered
+        this.props.history.push(`/store/${storeName}`);
+    };
     render() {
-        return ( 
-        <React.Fragment>{/*this is used to return sibling elements,otherwise you'd have to use a dummy div.*/}
-        <p>Fish!</p>
-        <form className="store-selector">
-        <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-        <button type="submit">Visit Store -></button>
-        </form>
-        </React.Fragment>
+        return (                 
+        <form className="store-selector" onSubmit={this.goToStore}>
+            <h2>Please Enter A Store</h2>
+            <input 
+            type="text" 
+            ref={this.myInput} 
+            required 
+            placeholder="Store Name" 
+            defaultValue={getFunName()} 
+            />
+            <button type="submit">Visit Store -></button>
+        </form>        
         )
     }
 }
