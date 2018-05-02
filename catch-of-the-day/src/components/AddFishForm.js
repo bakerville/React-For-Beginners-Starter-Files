fd83f1a5ee5ff5b9f1f80ac378a418b4f6399fc6
rlauderdale <rjlauderdale26@gmail.com>
2018-05-01 21:06:23 -0500

    AddFishForm
    Started the AddFishForm component and imported it into the Inventory.js

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+    createFish = (event) =>{
+        //1. Stop the form from submitting
+        event.preventDefault();
+        console.log('MAKING A FISH +fish');
+    }
+    render() {
+        return (
+            <form className="fish-edit" onSubmit={this.createFish}>
+              <input name="name" type= "text" placeholder="Name"/>
+              <input name="price" type= "text" placeholder="Price"/>
+                <select name="status"> 
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+
+                <textarea name= "desc" placeholder="Desc"/>
+                <input name= "image" type= "text" placeholder="Image"/>
+                <button type="submit">+ Add Fish</button>
+            </form>            
+        )
+    }
+}
+//Stopped video#13 at 10:05
+export default AddFishForm;
