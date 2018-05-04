ddb6e076b68d8e4b50510180cc8d12b59498cdd6
rlauderdale <rjlauderdale26@gmail.com>
2018-05-04 17:21:46 -0500

    updated state
    created Fish.js to render out the image, name, price, description, and status of the fish.
    
    updated App.js and Fish.js with the logic to render a functional addToOrder button.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component{
     state = {
@@ -23,11 +24,29 @@ class App extends React.Component{
     loadSampleFishes = () => {
         this.setState({ fishes: sampleFishes});
     };
+
+    addToOrder = (key) => {
+        // 1. take a copy of state
+        const order = { ...this.state.order};
+        // 2. Either add to the order, or update the number in our order
+        order[key] = order[key] + 1 || 1;
+        // 3. Call setState to update our state object
+        this.setState({ order }); //order: order
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header tagline="Fresh Seafood Market"/>                    
+                    <Header tagline="Fresh Seafood Market"/>
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => (
+                        <Fish
+                            key={key}
+                            index={key}
+                            details= {this.state.fishes[key]}
+                            addToOrder={this.addToOrder} />))}
+                    </ul>                        
                 </div>
                     <Order />
                     <Inventory 
