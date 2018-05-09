e283b952e1cfafc9d96381069aa6f5171fdd8865
rlauderdale <rjlauderdale26@gmail.com>
2018-05-09 08:04:09 -0500

    data flow
    setting up editFishForm component

---
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class EditFishForm extends React.Component {
+    render() {
+        return <div className ="fish-edit">
+            className
+            price
+            status
+            desc
+            image
+        </div>
+    }//stopped at video 20, 2:55 min in
+}
+
+
+export default EditFishForm;
