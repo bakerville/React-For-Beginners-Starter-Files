83849a05673cd2af0b1a5fdf2f533a588062014c
bakerville <thebakeriscomingforu@gmail.com>
2018-05-09 18:11:10 -0500

    updating inventory.js

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -2,15 +2,21 @@ import React from "react";
 
 class EditFishForm extends React.Component {
     render() {
-        return <div className ="fish-edit">
-            className
-            price
-            status
-            desc
-            image
+        return (
+        <div className ="fish-edit">
+            <input type="text" name="name" value={this.props.fish.name}/>
+            <input type="text" name="price" value={this.props.fish.price}/>
+            <select type="text" name="status" value={this.props.fish.status}>
+                <option value="available">Fresh!</option>
+                <option value="unavailable">Sold Out!</option>
+            </select>
+            <textarea name="desc" value={this.props.fish.desc} />
+            <input type="text" name="image" value={this.props.fish.image}/>
         </div>
-    }//stopped at video 20, 2:55 min in
+        );
+    }//stopped video at 6:48
 }
+
 
 
 export default EditFishForm;
