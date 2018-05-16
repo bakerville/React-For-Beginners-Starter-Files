1a366a1f18a2994e75e839821c8119a8af970bd0
bakerville <thebakeriscomingforu@gmail.com>
2018-05-15 21:45:43 -0500

    PropTypes
    imported PropTypes into the AddFishForm.js, App.js, EditFishForm.js, Fish.js, Header.js, Inventory.js, Order.js, and StorePicker.js.
    
    In addition I added static PropTypes into each file and listed the data types expected for each prop. This is an effort to validate the component.

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -14,28 +14,10 @@ const Header = (props) =>
             <span>{props.tagline}</span>
         </h3>
         </header> 
-    );
+);
 /* above is an implicit return, used with arrow functions*/
-
-
-
-
-//class Header extends React.Component {
- //   render() {
-  //      return (
-  //          <header className="top">
- //           <h1>Catch 
- //               <span className="ofThe"> 
-  //              <span className="of">Of</span>
-  //              <span className="the">The</span>
-  //              </span> 
-  //              Day</h1>
-  //          <h3 className="tagline">
-  //              <span>{this.props.tagline}</span>
-  //          </h3>
-  //          </header>            
-  //      )
- //   }
-//}
+Header.propTypes = {
+    tagline: PropTypes.string.isRequired
+};
 
 export default Header;
